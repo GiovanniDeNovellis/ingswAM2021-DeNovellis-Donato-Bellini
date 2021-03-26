60a8a8156570a55922a06d7159921c7ff9526e74
EmanueleBellini99 <79840467+EmanueleBellini99@users.noreply.github.com>
2021-03-26 19:35:19 +0100

    updated pom and uml in date 26/03/2021

--- src/test/java/it/polimi/ingsw/AppTest.java
+++ src/test/java/it/polimi/ingsw/AppTest.java
@@ -1,17 +1,11 @@
 package it.polimi.ingsw;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+
     @Test
     public void shouldAnswerWithTrue()
     {
